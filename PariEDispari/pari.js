037b77777b2a745a95458c69c4c480937cb3669a
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-08 16:33:40 +0100

    generate random number

--- PariEDispari/pari.js
+++ PariEDispari/pari.js
@@ -5,3 +5,10 @@ const choiceUser = prompt("Scegli: pari o dispari");
 // chiedo all'untente di inserire un numero da 1 a 5
 
 const numUser = parseInt(prompt("inserisci un numero da 1 a 5"));
+
+// genero un numero randomico da 1 a 5 con una funzione
+
+function generateNumberRandom(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
