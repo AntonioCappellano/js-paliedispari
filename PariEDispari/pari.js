50134180c9a487340d4d2dd518f746ae4591aeae
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-08 16:26:45 +0100

    choise number

--- PariEDispari/pari.js
+++ PariEDispari/pari.js
@@ -1,3 +1,7 @@
 // chiedo all'utente di scegliere pari o dispari
 
 const choiceUser = prompt("Scegli: pari o dispari");
+
+// chiedo all'untente di inserire un numero da 1 a 5
+
+const numUser = parseInt(prompt("inserisci un numero da 1 a 5"));
