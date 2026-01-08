fcb4d2df3920828ad4457f5bfe4ddedb48d6856f
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-08 15:32:20 +0100

    lowercase word

--- Palindromi/palindromi.js
+++ Palindromi/palindromi.js
@@ -1,2 +1,9 @@
 // Chiedo all'utente di inserire una parola
 const wordUser = prompt("Inserisci una parola");
+
+// Funzione per capire se la parola è palindroma
+
+function isWord(word) {
+  // rendo tutte le parole inserire minuscole
+  const lowerWord = word.toLowerCase();
+}
